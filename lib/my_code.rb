45583fba0609a92b637dd9f4b80f36eb2743816a
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-08-01 13:03:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,7 +62,7 @@ def reduce_to_any_true (source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push(if source_array == true)
+    new.push(if source_array == true)/n
     i += 1
   end
   return new
