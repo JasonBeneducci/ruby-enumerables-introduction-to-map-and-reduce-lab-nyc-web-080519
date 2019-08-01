45f71a95222b3fde97e2d1391fd3d94df7b31550
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-08-01 13:02:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,29 +40,29 @@ end
 
 def reduce_to_total(starting_ point, source_array)
   new = []
+  i = starting_point
+  while i < source_array.length do
+    new.push( source_array[i] + source_array[i+1] )
+    i += 1
+  end
+  return new[-1]
+end
+
+def reduce_to_all_true (source_array)
+  new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] )
+    new.push(if source_array == true )
     i += 1
   end
   return new
 end
 
-def reduce_all_true (source_array)
+def reduce_to_any_true (source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] )
-    i += 1
-  end
-  return new
-end
-
-def reduce_any_true (source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * source_array[i] )
+    new.push(if source_array == true)
     i += 1
   end
   return new
